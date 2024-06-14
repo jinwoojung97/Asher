6d49329d1521711304d1671cb9523eb7662ac9b1
Swift_Chu <chuyeonhak@nate.com>
2024-06-14 17:25:54 +0900

    Feat: Add function addBorder to extension View

--- Asher/Utils/Extensions/View +.swift
+++ Asher/Utils/Extensions/View +.swift
@@ -5,4 +5,12 @@
 //  Created by chuchu on 6/14/24.
 //
 
-import Foundation
+import SwiftUI
+
+extension View {
+    public func addBorder<S>(_ content: S, width: CGFloat = 1, cornerRadius: CGFloat) -> some View where S : ShapeStyle {
+        let roundedRect = RoundedRectangle(cornerRadius: cornerRadius)
+        return clipShape(roundedRect)
+            .overlay(roundedRect.strokeBorder(content, lineWidth: width))
+    }
+}
