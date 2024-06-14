fe41a8234b16b925a4e8eb3e32c6f07f8fe13466
Swift_Chu <chuyeonhak@nate.com>
2024-06-14 17:28:13 +0900

    Feat: Add HomeFeature

--- Asher/HomeTab/Home/HomeFeature.swift
+++ Asher/HomeTab/Home/HomeFeature.swift
@@ -5,4 +5,26 @@
 //  Created by chuchu on 6/14/24.
 //
 
-import Foundation
+import SwiftUI
+
+import ComposableArchitecture
+
+struct HomeFeature: Reducer {
+  struct State: Equatable {
+    var name: String
+    var calendarHeight: CGFloat = .zero
+  }
+  
+  enum Action: Equatable {
+    case setCalendarHeight(CGFloat)
+  }
+  
+  func reduce(into state: inout State, action: Action) -> Effect<Action> {
+    switch action {
+    case .setCalendarHeight(let height):
+      state.calendarHeight = height
+      
+      return .none
+    }
+  }
+}
