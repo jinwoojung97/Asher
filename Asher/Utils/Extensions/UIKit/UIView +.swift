5b5e3d5bd0c677eb5171b635bb6d78a4d0978b98
Swift_Chu <chuyeonhak@nate.com>
2024-06-14 17:27:11 +0900

    Feat: Add function addBorder to Extension UIView

--- Asher/Utils/Extensions/UIKit/UIView +.swift
+++ Asher/Utils/Extensions/UIKit/UIView +.swift
@@ -43,6 +43,26 @@ public extension UIView {
         layer.borderWidth = width
     }
     
+    func addBorder(_ arr_edge: [UIRectEdge], color: UIColor, width: CGFloat) {
+        for edge in arr_edge {
+            let border = CALayer()
+            
+            switch edge {
+            case UIRectEdge.top:
+                border.frame = CGRect(x: 0, y: 0, width: frame.width, height: width)
+            case UIRectEdge.bottom:
+                border.frame = CGRect(x: 0, y: frame.height - width, width: frame.width, height: width)
+            case UIRectEdge.left:
+                border.frame = CGRect(x: 0, y: 0, width: width, height: frame.height)
+            case UIRectEdge.right:
+                border.frame = CGRect(x: frame.width - width, y: 0, width: width, height: frame.height)
+            default: break
+            }
+            border.backgroundColor = color.cgColor
+            layer.addSublayer(border)
+        }
+    }
+    
     func shakeAnimation(completion: (() -> ())? = nil) {
         let animation = CAKeyframeAnimation(keyPath: "transform.translation.x")
         animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
