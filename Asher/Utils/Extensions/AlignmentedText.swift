b4e1e089330fa48e66fd7abe7ea237af60029db4
Swift_Chu <chuyeonhak@nate.com>
2024-06-14 17:27:30 +0900

    Feat: Add AlignmentedText

--- Asher/Utils/Extensions/AlignmentedText.swift
+++ Asher/Utils/Extensions/AlignmentedText.swift
@@ -5,4 +5,34 @@
 //  Created by chuchu on 6/14/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct AlignmentedText: View {
+    private let text: String
+    private let alignment: Alignment
+    private let spacing: CGFloat
+    
+    init(
+        text: String,
+        alignment: Alignment = .leading,
+        spacing: CGFloat = 16.0
+    ) {
+        self.alignment = alignment
+        self.text = text
+        self.spacing = spacing
+    }
+    
+    var edge: Edge.Set {
+        switch alignment {
+        case .leading: return .leading
+        case .trailing: return .trailing
+        default: return .leading
+        }
+    }
+    
+    var body: some View {
+        Text(text)
+            .frame(maxWidth: .infinity, alignment: alignment)
+            .padding(edge, spacing)
+    }
+}
