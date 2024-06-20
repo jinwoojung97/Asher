9331235ba24e59ac0e0a7821a08dcdca56bce5f8
Swift_Chu <chuyeonhak@nate.com>
2024-06-20 10:56:04 +0900

    Feat: Add DayCategory to date

--- Asher/Utils/Extensions/Date +.swift
+++ Asher/Utils/Extensions/Date +.swift
@@ -7,8 +7,7 @@
 
 import Foundation
 
-public extension Date {
-    
+extension Date {
     static var currentMonth: Date {
         guard case let calendar = Calendar.current,
               case let components = calendar.dateComponents([.month, .year], from: .now),
@@ -16,6 +15,17 @@ public extension Date {
         else { return .now }
         
         return currentMonth
+    }
+    
+    var category: DayCategory? {
+        let calendar = Calendar.current
+        if calendar.isDateInToday(self) {
+            return .today
+        } else if let yesterday = calendar.date(byAdding: .day, value: -1, to: Date()),
+                    calendar.isDate(self, inSameDayAs: yesterday) {
+            return .yesterday
+        }
+        return nil
     }
     
     func toString() -> String {
@@ -34,5 +44,23 @@ public extension Date {
         formatter.locale = .current
         
         return Int(Int(formatter.string(from: self)) ?? 0)
+    }
+}
+
+extension Date {
+    enum DayCategory {
+        case yesterday
+        case today
+        
+        var toString: String {
+            switch self {
+            case .yesterday: "어제"
+            case .today: "오늘"
+            }
+        }
+        
+        var title: String {
+            "\(toString)의 기분을 체크해보세요!"
+        }
     }
 }
