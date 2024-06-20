a6efae284104c8ff1b19c7e659a433c0e67008a8
Swift_Chu <chuyeonhak@nate.com>
2024-06-20 10:52:09 +0900

    Feat: Add LoopingScrollView

--- Asher/HomeTab/LoopingScrollView.swift
+++ Asher/HomeTab/LoopingScrollView.swift
@@ -5,4 +5,99 @@
 //  Created by chuchu on 6/19/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct LoopingScrollView<Content: View, Item: RandomAccessCollection>: View where Item.Element: Identifiable {
+    var width: CGFloat
+    var spacing: CGFloat = 0.0
+    var items: Item
+    @ViewBuilder var content: (Item.Element) -> Content
+    var body: some View {
+        GeometryReader {
+            let size = $0.size
+            // 안전성 체크
+            let repaetingCount = width > 0 ?  Int((size.width / width).rounded()) + 1: 1
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHStack(spacing: 0) {
+                    ForEach(items) { item in
+                        content(item)
+                            .padding([.horizontal, .vertical], spacing)
+                            .frame(width: width)
+                    }
+                    
+                    ForEach(0..<repaetingCount, id: \.self) { index in
+                        let item = Array(items)[index % items.count]
+                        content(item)
+                            .padding([.horizontal, .vertical], spacing)
+                            .frame(width: width)
+                    }
+                }
+                .background {
+                    ScrollViewHelper(
+                        width: width,
+                        spacing: spacing,
+                        itemsCount: items.count,
+                        repeatingCount: repaetingCount)
+                }
+            }
+        }
+    }
+}
+
+fileprivate struct ScrollViewHelper: UIViewRepresentable {
+    var width: CGFloat
+    var spacing: CGFloat
+    var itemsCount: Int
+    var repeatingCount: Int
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(
+            width: width,
+            spacing: spacing,
+            itemsCount: itemsCount,
+            repeatingCount: repeatingCount)
+    }
+    
+    func makeUIView(context: Context) -> UIView { .init() }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.06) {
+            if let scrollView = uiView.superview?.superview?.superview as? UIScrollView,
+                !context.coordinator.isAdded {
+                scrollView.delegate = context.coordinator
+                context.coordinator.isAdded = true
+            }
+        }
+    }
+    
+    class Coordinator: NSObject, UIScrollViewDelegate {
+        var width: CGFloat
+        var spacing: CGFloat
+        var itemsCount: Int
+        var repeatingCount: Int
+        var isAdded: Bool = false
+        
+        init(width: CGFloat, spacing: CGFloat, itemsCount: Int, repeatingCount: Int) {
+            self.width = width
+            self.spacing = spacing
+            self.itemsCount = itemsCount
+            self.repeatingCount = repeatingCount
+        }
+        
+        func scrollViewDidScroll(_ scrollView: UIScrollView) {
+            let minX = scrollView.contentOffset.x
+            let mainContentSize = CGFloat(itemsCount) * (width - spacing)
+            let spacingSize = CGFloat(itemsCount) * spacing
+            
+            if minX > (mainContentSize + spacingSize) {
+                scrollView.contentOffset.x -= mainContentSize + spacingSize
+            }
+            
+            if minX < 0 {
+                scrollView.contentOffset.x += mainContentSize + spacingSize
+            }
+        }
+     }
+}
+
