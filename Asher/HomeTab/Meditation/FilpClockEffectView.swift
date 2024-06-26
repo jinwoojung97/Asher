b487fc60ef3286e1e953d19bbae8aee1aa0425ee
Swift_Chu <chuyeonhak@nate.com>
2024-06-26 17:25:13 +0900

    Feat: Add FlipClockEffectView

---
+++ Asher/HomeTab/Meditation/FilpClockEffectView.swift
@@ -0,0 +1,18 @@
+//
+//  FilpClockEffectView.swift
+//  Asher
+//
+//  Created by chuchu on 6/26/24.
+//
+
+import SwiftUI
+
+struct FlipClockEffectView: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+#Preview {
+    FlipClockEffectView()
+}
---
+++ Asher/HomeTab/Meditation/MeditationView.swift
@@ -0,0 +1,8 @@
+//
+//  MeditationView.swift
+//  Asher
+//
+//  Created by chuchu on 6/26/24.
+//
+
+import Foundation
