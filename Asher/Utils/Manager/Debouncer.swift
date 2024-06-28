c775f555d29fb20277aa95e109f9ee395014365e
Swift_Chu <chuyeonhak@nate.com>
2024-06-28 14:32:13 +0900

    Feat: Add Debouncer

--- Asher/Utils/Manager/Debouncer.swift
+++ Asher/Utils/Manager/Debouncer.swift
@@ -5,4 +5,18 @@
 //  Created by chuchu on 6/28/24.
 //
 
-import Foundation
+import SwiftUI
+import Combine
+
+class Debouncer: ObservableObject {
+  @Published var trigger = PassthroughSubject<Void, Never>()
+  var handleTrigger: (() -> ())?
+  private var cancellables = Set<AnyCancellable>()
+  
+  init() {
+    trigger
+      .debounce(for: .seconds(1), scheduler: RunLoop.main)
+      .sink { $0 }
+      .store(in: &cancellables)
+  }
+}
