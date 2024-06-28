ef115e14d2d93e1a6e79453accc08d813cc25431
Swift_Chu <chuyeonhak@nate.com>
2024-06-28 14:35:17 +0900

    Fix: Prevent preview bug

--- Asher/SwiftData/ContentView.swift
+++ Asher/SwiftData/ContentView.swift
@@ -8,66 +8,66 @@
 import SwiftUI
 import SwiftData
 
-struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
-    var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        if let mood = item.mood {
-                            Text(mood.emoji)
-                        }
-                    } label: {
-                        Text(item.date)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: {
-                        addItem(date: .now)
-                    }, label: {
-                        Label("Add Item", systemImage: "plus")
-                    })
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
-
-    private func addItem(date: Date) {
-        let dayString = date.toDayString()
-        let fetchPredicate = #Predicate<Item> { $0.date == dayString }
-        let descriptor = FetchDescriptor(predicate: fetchPredicate)
-        do {
-            if let date = try modelContext.fetch(descriptor).first {
-                date.mood = .sad
-            } else {
-                let newItem = Item(date: dayString, mood: .happy)
-                modelContext.insert(newItem)
-            }
-        } catch {
-            print(error.localizedDescription)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
-    }
-}
-
+//struct ContentView: View {
+//    @Environment(\.modelContext) private var modelContext
+//    @Query private var items: [Item]
+//
+//    var body: some View {
+//        NavigationSplitView {
+//            List {
+//                ForEach(items) { item in
+//                    NavigationLink {
+//                        if let mood = item.mood {
+//                            Text(mood.emoji)
+//                        }
+//                    } label: {
+//                        Text(item.date)
+//                    }
+//                }
+//                .onDelete(perform: deleteItems)
+//            }
+//            .toolbar {
+//                ToolbarItem(placement: .navigationBarTrailing) {
+//                    EditButton()
+//                }
+//                ToolbarItem {
+//                    Button(action: {
+//                        addItem(date: .now)
+//                    }, label: {
+//                        Label("Add Item", systemImage: "plus")
+//                    })
+//                }
+//            }
+//        } detail: {
+//            Text("Select an item")
+//        }
+//    }
+//
+//    private func addItem(date: Date) {
+//        let dayString = date.toDayString()
+//        let fetchPredicate = #Predicate<Item> { $0.date == dayString }
+//        let descriptor = FetchDescriptor(predicate: fetchPredicate)
+//        do {
+//            if let date = try modelContext.fetch(descriptor).first {
+//                date.mood = .sad
+//            } else {
+//                let newItem = Item(date: dayString, mood: .happy)
+//                modelContext.insert(newItem)
+//            }
+//        } catch {
+//            print(error.localizedDescription)
+//        }
+//    }
+//
+//    private func deleteItems(offsets: IndexSet) {
+//        withAnimation {
+//            for index in offsets {
+//                modelContext.delete(items[index])
+//            }
+//        }
+//    }
+//}
+//
 //#Preview {
 //    ContentView()
 //        .modelContainer(for: Item.self, inMemory: true)
