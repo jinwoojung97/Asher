771a4b79828a61225cbfd92c289d42c9295078f2
Swift_Chu <chuyeonhak@nate.com>
2024-06-28 14:32:32 +0900

    Feat: Add CustomHeaderView

--- Asher/Utils/Extensions/CustomDesign/Header/CustomHeaderView.swift
+++ Asher/Utils/Extensions/CustomDesign/Header/CustomHeaderView.swift
@@ -5,4 +5,30 @@
 //  Created by chuchu on 6/28/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct CustomHeaderView: View {
+  let action: () -> ()
+  let title: String
+  
+  var body: some View {
+    ZStack {
+      HStack {
+        Button(action: action, label: {
+          Image(.back)
+        })
+        .frame(width: 48, height: 48)
+        .padding(.leading, 8)
+        
+        Spacer()
+      }
+      
+      
+      Text(title)
+        .font(.notoSans(width: .bold, size: 18))
+        .foregroundStyle(.subtitleOn)
+    }
+    Spacer()
+  }
+}
+
