e8441896c93a950f471330840e0976d037a06fc8
Swift_Chu <chuyeonhak@nate.com>
2024-06-28 14:31:56 +0900

    Feat: Add Throttler

--- Throttler.swift
+++ Throttler.swift
@@ -5,4 +5,20 @@
 //  Created by chuchu on 6/28/24.
 //
 
-import Foundation
+import SwiftUI
+import Combine
+
+class Throttler: ObservableObject {
+  @Published var trigger = PassthroughSubject<Void, Never>()
+  var handleTrigger: (() -> ())?
+  private var cancellables = Set<AnyCancellable>()
+  
+  init(for time: RunLoop.SchedulerTimeType.Stride) {
+    trigger
+      .throttle(for: time, scheduler: RunLoop.main, latest: false)
+      .sink { [weak self] _ in self?.handleTrigger?() }
+      .store(in: &cancellables)
+  }
+  
+  deinit { print("Throttler deinit") }
+}
