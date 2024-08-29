40a389ce1f12fe5bdf8c8edc4d90e3f651d559cc
Swift_Chu <chuyeonhak@nate.com>
2024-08-29 16:39:13 +0900

    Feat: Add Screen lock feature

--- Asher/AsherApp.swift
+++ Asher/AsherApp.swift
@@ -13,29 +13,47 @@ import FirebaseCrashlytics
 
 @main
 struct AsherApp: App {
-//    var sharedModelContainer: ModelContainer = {
-//        let schema = Schema([
-//            Item.self,
-//        ])
-//        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-//
-//        do {
-//            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-//        } catch {
-//            fatalError("Could not create ModelContainer: \(error)")
-//        }
-//    }()
+    //    var sharedModelContainer: ModelContainer = {
+    //        let schema = Schema([
+    //            Item.self,
+    //        ])
+    //        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+    //
+    //        do {
+    //            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+    //        } catch {
+    //            fatalError("Could not create ModelContainer: \(error)")
+    //        }
+    //    }()
     
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-
+    @Environment(\.scenePhase) var scenePhase
+    @State var isStart = true
+    
     var body: some Scene {
         WindowGroup {
             RootViewControllerRepresentable()
                 .ignoresSafeArea(.all)
-//            ContentView()
+                .onReceive(NotificationCenter.default.publisher(for: UIScene.willEnterForegroundNotification)) { _ in
+                    delegate.openLockScreen()
+                }
+                .onChange(of: scenePhase) { newScenePhase in
+                    switch newScenePhase {
+                    case .active where isStart:
+                        delegate.openLockScreen()
+                        isStart = false
+                        print("App is active")
+                    case .inactive:
+                        print("App is inactive")
+                    case .background:
+                        print("App is in background")
+                    @unknown default:
+                        print("unexpected Value")
+                    }
+                }
+            //            ContentView()
             
         }
-//        .modelContainer(SwiftDataModelConfigurationProvider.shared.container)
     }
 }
 
@@ -43,5 +61,22 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
         return true
+    }
+    
+    func openLockScreen() {
+        print(#function)
+        let window = UIApplication.shared.window
+        let lock = window?.rootViewController?.presentedViewController as? LockScreenViewController
+        let rootViewController = window?.rootViewController as? RootViewController
+        
+        guard UserDefaultsManager.shared.usePassword && lock == nil
+        else { return }
+        
+        print("my password = \(UserDefaultsManager.shared.password)")
+        let lockScreenVc = LockScreenViewController(type: .normal)
+        
+        lockScreenVc.modalPresentationStyle = .overFullScreen
+        
+        UIApplication.shared.window?.rootViewController?.present(lockScreenVc, animated: false)
     }
 }
---
+++ Asher/Setting/ScreenLock/ScreenLockFeature.swift
@@ -0,0 +1,8 @@
+//
+//  ScreenLockFeature.swift
+//  Asher
+//
+//  Created by chuchu on 8/28/24.
+//
+
+import Foundation
