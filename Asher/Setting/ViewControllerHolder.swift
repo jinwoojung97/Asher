313516deb181b6f2b4ab07758df4d7e6dc9c14ef
추연학 <=>
2024-09-12 16:59:38 +0900

    Feat: Add ViewControllerHolder

--- Asher/Setting/ViewControllerHolder.swift
+++ Asher/Setting/ViewControllerHolder.swift
@@ -5,4 +5,37 @@
 //  Created by chuchu on 8/28/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct ViewControllerHolder {
+    weak var value: UIViewController?
+}
+
+struct ViewControllerKey: EnvironmentKey {
+    static var defaultValue: ViewControllerHolder {
+        return ViewControllerHolder(value: UIApplication.shared.window?.rootViewController)
+
+    }
+}
+
+extension EnvironmentValues {
+    var viewController: UIViewController? {
+        get { return self[ViewControllerKey.self].value }
+        set { self[ViewControllerKey.self].value = newValue }
+    }
+}
+
+extension UIViewController {
+    func present<Content: View>(style: UIModalPresentationStyle = .automatic, @ViewBuilder builder: () -> Content) {
+        let toPresent = UIHostingController(rootView: AnyView(EmptyView()))
+        toPresent.modalPresentationStyle = style
+        toPresent.rootView = AnyView(
+            builder()
+                .environment(\.viewController, toPresent)
+        )
+        NotificationCenter.default.addObserver(forName: Notification.Name(rawValue: "dismissModal"), object: nil, queue: nil) { [weak toPresent] _ in
+            toPresent?.dismiss(animated: true, completion: nil)
+        }
+        self.present(toPresent, animated: true, completion: nil)
+    }
+}
