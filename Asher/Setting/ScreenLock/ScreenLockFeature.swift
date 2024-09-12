9a71b6bb6f1d08bbea7d6e51f3d19971b608423f
추연학 <=>
2024-09-12 16:59:14 +0900

    Feat: Add Lock Feature

--- Asher/Setting/ScreenLock/ScreenLockFeature.swift
+++ Asher/Setting/ScreenLock/ScreenLockFeature.swift
@@ -10,18 +10,23 @@ import SwiftUI
 import ComposableArchitecture
 
 struct ScreenLockFeature: Reducer {
+  let auth = BiometricsAuthManager()
   struct State: Equatable {
     var usePassword = UserDefaultsManager.shared.usePassword
     var useBiometricsAuth = UserDefaultsManager.shared.useBiometricsAuth
     var openLockScreen = false
+    var showErrorAlert = false
+    var openChangePassword = false
   }
   
   enum Action: Equatable {
-    
     case setUsePassword(Bool)
     case setBiometricsAuth(Bool)
     case openLockScreen
+    case openChangePassword
     case tapChangePassword
+    case lockAction(BiometricsAuthManager.AuthenticationState)
+    case openAlert
   }
   
   func reduce(into state: inout State, action: Action) -> Effect<Action> {
@@ -30,21 +35,51 @@ struct ScreenLockFeature: Reducer {
       if usePassword { state.openLockScreen = true }
       state.usePassword = usePassword
       UserDefaultsManager.shared.usePassword = usePassword
+      
       return .none
       
     case .setBiometricsAuth(let useBiometricsAuth):
-      state.useBiometricsAuth = useBiometricsAuth
-      UserDefaultsManager.shared.useBiometricsAuth = useBiometricsAuth
-      BiometricsAuthManager().execute()
-      return .none
+      if useBiometricsAuth {
+        return .run { send in
+          await send(.lockAction(auth.execute()))
+        }
+      }
+      else {
+        setUseBiometricsAuth(into: &state, canUse: false)
+        return .none
+      }
       
     case .openLockScreen:
       state.openLockScreen.toggle()
       return .none
+    case .openChangePassword:
+      state.openChangePassword.toggle()
+      return .none
     case .tapChangePassword:
       state.openLockScreen.toggle()
       return .none
+    case .lockAction(let action):
+      switch action {
+      case .loggedIn:
+        setUseBiometricsAuth(into: &state, canUse: true)
+        
+      case .fail(_):
+        setUseBiometricsAuth(into: &state, canUse: false)
+        state.showErrorAlert = true
+      }
+      return .none
+    case .openAlert:
+      state.showErrorAlert = false
+      return .none
     }
   }
+  
+  private func test() {
+    
+  }
+  
+  private func setUseBiometricsAuth(into state: inout State, canUse: Bool) {
+    state.useBiometricsAuth = canUse
+    UserDefaultsManager.shared.useBiometricsAuth = canUse
+  }
 }
-
--- Asher/Setting/ScreenLock/ScreenLockView.swift
+++ Asher/Setting/ScreenLock/ScreenLockView.swift
@@ -5,4 +5,67 @@
 //  Created by chuchu on 8/28/24.
 //
 
-import Foundation
+import SwiftUI
+
+import ComposableArchitecture
+
+struct ScreenLockView: View {
+  let customHeaderView: CustomHeaderView
+  @State var store = Store(initialState: ScreenLockFeature.State()) { ScreenLockFeature() }
+  let auth = BiometricsAuthManager()
+  
+  init(customHeaderView: CustomHeaderView) {
+    self.customHeaderView = customHeaderView
+  }
+  
+  var body: some View {
+    WithViewStore(store, observe: { $0 }) { viewStore in
+      customHeaderView
+      GeometryReader { _ in
+        VStack {
+          Toggle(isOn: viewStore.binding(get: \.usePassword, send: { .setUsePassword($0) })) {
+            Text("잠금 설정")
+          }
+          .padding(.horizontal)
+          .padding(.bottom, 4)
+          
+          Divider()
+          
+          if viewStore.usePassword {
+            Toggle(isOn: viewStore.binding(get: \.useBiometricsAuth, send: { .setBiometricsAuth($0) })) {
+              Text("생체 인증")
+            }
+            .padding(.horizontal)
+            .padding(.vertical, 8)
+            
+            Button(action: {
+              viewStore.send(.openChangePassword)
+            }) {
+              Text("비밀번호 재설정")
+            }
+            .padding(.horizontal)
+            .padding(.vertical, 8)
+            .background(.border)
+            .cornerRadius(8)
+            
+          }
+        }
+        .fullScreenCover(isPresented: viewStore.binding(get: \.openLockScreen, send: { _ in .openLockScreen })) {
+          LockScreenViewControllerWrapper(lockType: .newPassword) { didUnlock in
+            viewStore.send(.setUsePassword(didUnlock))
+          }
+        }
+        .fullScreenCover(isPresented: viewStore.binding(get: \.openChangePassword, send: { _ in .openChangePassword })) {
+          LockScreenViewControllerWrapper(lockType: .changePassword) { _ in }
+        }
+        .alert("메시지", isPresented: viewStore.binding(get: \.showErrorAlert, send: { _ in .openAlert })) {
+          Button("설정", role: .cancel) {
+            if let url = URL(string: UIApplication.openSettingsURLString) {
+                UIApplication.shared.open(url)
+            }
+          }
+        }
+      }
+    }
+  }
+}
