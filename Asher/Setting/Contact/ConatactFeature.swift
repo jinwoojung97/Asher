155ec0b4d001c3959d0ebc9be93c3e3085588999
추연학 <=>
2024-09-23 16:50:02 +0900

    Feat: Add Contact Feature

--- Asher/Setting/Contact/ConatactFeature.swift
+++ Asher/Setting/Contact/ConatactFeature.swift
@@ -5,4 +5,112 @@
 //  Created by chuchu on 9/9/24.
 //
 
-import Foundation
+import SwiftUI
+
+import ComposableArchitecture
+
+@Reducer
+struct ConatactFeature {
+  
+  @ObservableState
+  struct State: Equatable {
+    var email = ""
+    var contents = ""
+    var category: ContactCategory?
+  }
+  
+  enum Action: BindableAction {
+    case binding(BindingAction<State>)
+    case sendButton
+    case selectCategory(ContactCategory)
+  }
+  
+  var body: some ReducerOf<Self> {
+    BindingReducer()
+    Reduce { state, action in
+      switch action {
+      case .binding(\.email):
+        
+        print("testField, \(state.email)")
+        return .none
+        
+      case .binding(\.contents):
+        print("testField, \(state.contents)")
+        
+        return .none
+        
+      case .sendButton:
+        do {
+          try validCheck(state: state)
+          submitInquiry(state: state)
+        } catch(let error) {
+          Toast.shared.present(toastItem: ToastItem(title: error.localizedDescription))
+        }
+        return .none
+      case .selectCategory(let category):
+        state.category = category
+        return .none
+        
+      case .binding(_): return .none
+      }
+    }
+  }
+  
+  private func validCheck(state: State) throws {
+    if state.email.isEmpty { throw ContactError.emailIsEmpty }
+    if state.contents.isEmpty { throw ContactError.contentsIsEmpty }
+    if state.category == nil { throw ContactError.nonSelectedCategory }
+  }
+  
+  private func submitInquiry(state: State) {
+    guard let title = state.category?.title else { return }
+      IndicatorManager.shared.startAnimation()
+      let messageModel = SlackMessageModel()
+      .addBlock(blockType: .header(title))
+          .addBlock(blockType: .osType)
+          .addBlock(blockType: .subtitle(state.email))
+          .addBlock(blockType: .contents(state.contents))
+          .addBlock(blockType: .divider)
+      
+    NetworkManager.shared.request(api: .contactUs(messageModel)) { data in
+          IndicatorManager.shared.stopAnimation()
+          switch data.result {
+          case .success(_):
+            Toast.shared.present(toastItem: ToastItem(title: "success"))
+            NavigationManager.shared.pop()
+          case .failure(let error):
+              print(error)
+          }
+      }
+  }
+  
+  enum ContactError: Error, LocalizedError {
+    case emailIsEmpty
+    case contentsIsEmpty
+    case nonSelectedCategory
+    
+    var errorDescription: String? {
+      switch self {
+      case .emailIsEmpty: "이메일이나 연락처를 입력해주세요"
+      case .contentsIsEmpty: "내용을 입력해주세요"
+      case .nonSelectedCategory: "카테고리를 입력해주세요"
+      }
+    }
+  }
+  
+  
+}
+
+enum ContactCategory: CaseIterable {
+  case error
+  case suggestions
+  case etc
+  
+  var title: String {
+    switch self {
+    case .error: "🐞버그 발견🐞"
+    case .suggestions: "❗️건의사항❗️"
+    case .etc: "🎸 기타 🎸"
+    }
+  }
+}
--- Asher/Setting/Contact/ContactView.swift
+++ Asher/Setting/Contact/ContactView.swift
@@ -5,4 +5,84 @@
 //  Created by chuchu on 9/9/24.
 //
 
-import Foundation
+import SwiftUI
+
+import ComposableArchitecture
+
+struct ContactView: View {
+  @State var store: StoreOf<ConatactFeature>
+  let customHeaderView: CustomHeaderView
+  
+  var body: some View {
+    customHeaderView
+    
+    VStack {
+      wrapperView {
+        TextField(
+          "eamil",
+          text: $store.email,
+          prompt: Text("연락 받을 곳을 입력해주세요!")
+            .foregroundStyle(.subtitle)
+            .font(.notoSans(width: .regular, size: 16))
+        )
+        .font(.notoSans(width: .regular, size: 16))
+      }
+      .frame(height: 80)
+      
+      Menu (content: {
+        ForEach(ContactCategory.allCases, id: \.self) { category in
+          Button(action: { store.send(.selectCategory(category)) }) {
+            Text(category.title)
+              .font(.notoSans(width: .regular, size: 16))
+          }
+        }
+      }) {
+        wrapperView {
+          Text(store.state.category?.title ?? "카테고리를 선택해 주세요")
+            .font(.notoSans(width: .regular, size: 16))
+            .foregroundStyle(store.state.category == nil ? .subtitle: .subtitleOn)
+          
+        }
+        .frame(height: 80)
+      }
+      
+      wrapperView {
+        TextEditor(text: $store.contents)
+          .scrollContentBackground(.hidden)
+          .background(.clear)
+          .foregroundStyle(.subtitleOn)
+          .font(.notoSans(width: .regular, size: 16))
+      }
+      .frame(height: 144)
+      
+      RoundedRectangle(cornerRadius: 12)
+        .foregroundStyle(.button)
+        .overlay(alignment: .center) {
+          Text("보내기")
+            .font(.notoSans(width: .bold, size: 16))
+            .foregroundStyle(.white)
+            .padding(16)
+        }
+        .padding(.vertical, 12)
+        .padding(.horizontal, 16)
+        .onTapGesture { store.send(.sendButton) }
+        .frame(height: 72)
+      
+      Spacer()
+    }
+  }
+  
+  @ViewBuilder
+  func wrapperView<V>(
+    @ViewBuilder content: () -> V
+  ) -> some View where V : View {
+    RoundedRectangle(cornerRadius: 12)
+      .foregroundStyle(.main2)
+      .overlay(alignment: .leading) {
+        content()
+          .padding(16)
+      }
+      .padding(.vertical, 12)
+      .padding(.horizontal, 16)
+  }
+}
